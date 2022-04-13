4c7a793ab62d506723f8d4bcb7335b48350a9dab
ASWIN A K <aswiak799@gmail.com>
2022-04-13 12:57:01 +0530

    Str manipulation 13/04/2022

--- exercise4/StringManipulation.java
+++ exercise4/StringManipulation.java
@@ -20,17 +20,17 @@ class StrManipulation{
 
 
 		//toUpperCase and toLowerCase
-		System.out.println("---------string case change------------");
+		System.out.println("\n---------string case change------------\n");
 		System.out.println(s1+" toUpperCase: "+s1.toUpperCase());
 		System.out.println(s2+" toUpperCase: "+s2.toLowerCase());
 
-		System.out.println("---------string concatnation------------");
+		System.out.println("\n---------string concatnation------------\n");
 		System.out.println("using concat() :"+s1.concat(s2));
 		System.out.println("using + operator :"+ s1 + s2);
 
 
 		//character exetraction
-		System.out.println("---------character exetraction------------");
+		System.out.println("\n---------character exetraction------------\n");
 		
 		System.out.println("character at 3rd position:"+s1.charAt(3));
 		char c[] = new char[5];
@@ -38,7 +38,7 @@ class StrManipulation{
 		s1.getChars(2,4,c,0);
 		System.out.println("character between 2 and 4 :"+new String(c));
 
-		System.out.println("---------character comparison------------");
+		System.out.println("\n---------character comparison------------\n");
 		s1 = "welcome";
 		s2 = "WELCOME";
 		System.out.println(s1 +" Equals" +s2+" : "+s1.equals(s2));
@@ -51,20 +51,20 @@ class StrManipulation{
 		System.out.println(s1 +" endsWith x: "+s1.endsWith("x"));
 
 
-		System.out.println("---------Search Substring------------");
+		System.out.println("\n---------Search Substring------------\n");
 		s1= "hello alexa how how are you";
 		System.out.println(s1 +" indexOf(how) : "+s1.indexOf("how"));
 		System.out.println(s1 +" lastIndexOf(how) : "+s1.lastIndexOf("how"));
 
 
-		System.out.println("---------Modifying a string------------");
+		System.out.println("\n---------Modifying a string------------\n");
 		s1="welcome to java";
 		
 		System.out.println(s1+"<--> replace java to python: "+s1.replace("java","python"));
 
 
 
-		System.out.println("---------Data conversion using valueOf()------------");
+		System.out.println("\n---------Data conversion using valueOf()------------\n");
 		int i=10;
 		
 		System.out.println(i +" is covert using Data conversion using valueOf() : "+String.valueOf(i));
@@ -75,4 +75,51 @@ class StrManipulation{
 }
 
 
-}
+}/*
+
+output
+------
+New string from char[] using new string:welcome
+New string from byte[] using new string:ABCDEF
+string length of 'good': 4
+
+---------string case change------------
+
+good toUpperCase: GOOD
+Morning toUpperCase: morning
+
+---------string concatnation------------
+
+using concat() :goodMorning
+using + operator :goodMorning
+
+---------character exetraction------------
+
+character at 3rd position:d
+character between 2 and 4 :od
+
+---------character comparison------------
+
+welcome EqualsWELCOME : false
+welcome equalsIgnoreCaseWELCOME : true
+welcome compareToWELCOME : 32
+welcome compareToIgnoreCaseWELCOME : 0
+welcome startsWith w : true
+welcome endsWith x: false
+
+---------Search Substring------------
+
+hello alexa how how are you indexOf(how) : 12
+hello alexa how how are you lastIndexOf(how) : 16
+
+---------Modifying a string------------
+
+welcome to java<--> replace java to python: welcome to python
+
+---------Data conversion using valueOf()------------
+
+10 is covert using Data conversion using valueOf() : 10
+concat 100 with string 10:10010
+
+
+*/
